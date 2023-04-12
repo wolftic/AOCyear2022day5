0d4f0f5e4d14989bcb5f92d4567e8444e9a879f5
Lorenzo Kramer <lorenzokramertje@gmail.com>
2023-04-12 18:15:52 +0200

    Added a function to parse the input file data.

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,76 @@
 "use strict";
-console.log("hello world!");
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs = __importStar(require("fs"));
+const path = __importStar(require("path"));
+const [fileName] = process.argv.slice(2);
+const filePath = path.join(__dirname, "../data/", `${fileName}.txt`);
+function parseInput(inputContent) {
+    var _a, _b, _c;
+    const rows = inputContent.split("\n");
+    const structure = [];
+    const instructions = [];
+    let fillStructureRows = true;
+    for (let i = 0; i < rows.length; i++) {
+        const row = rows[i].trimEnd();
+        // row is empty
+        if (!row) {
+            fillStructureRows = !fillStructureRows;
+            continue;
+        }
+        if (fillStructureRows) {
+            const totalBoxes = Math.ceil(row.length / 4);
+            for (let j = 0; j < totalBoxes; j++) {
+                const pos = j + 1 + j * 3;
+                const letter = row[pos].trim();
+                const isCrate = row[pos - 1] === "[" && row[pos + 1] === "]";
+                if (!letter || !isCrate) {
+                    continue;
+                }
+                if (!structure[j]) {
+                    structure[j] = [];
+                }
+                structure[j].unshift(letter);
+            }
+        }
+        else {
+            const match = row.match(/move (?<amount>[0-9]+) from (?<from>[0-9]+) to (?<to>[0-9]+)/i);
+            if (!match) {
+                console.warn("WARN: invalid instruction at line:", i, `"${row}"`);
+                continue;
+            }
+            const instruction = {
+                amount: parseInt((_a = match === null || match === void 0 ? void 0 : match.groups) === null || _a === void 0 ? void 0 : _a.amount),
+                from: parseInt((_b = match === null || match === void 0 ? void 0 : match.groups) === null || _b === void 0 ? void 0 : _b.from),
+                to: parseInt((_c = match === null || match === void 0 ? void 0 : match.groups) === null || _c === void 0 ? void 0 : _c.to),
+            };
+            instructions.push(instruction);
+        }
+    }
+    return [structure, instructions];
+}
+const contents = fs.readFileSync(filePath, "utf-8");
+const [rows, instructions] = parseInput(contents);
+console.log(rows, instructions);
--- index.ts
+++ index.ts
@@ -1,1 +1,75 @@
-console.log("hello world!");
+import * as fs from "fs";
+import * as path from "path";
+
+const [fileName] = process.argv.slice(2);
+
+const filePath: string = path.join(__dirname, "../data/", `${fileName}.txt`);
+
+type Instruction = {
+  from: number;
+  to: number;
+  amount: number;
+};
+
+function parseInput(inputContent: string): [string[][], Instruction[]] {
+  const rows = inputContent.split("\n");
+
+  const structure: string[][] = [];
+  const instructions: Instruction[] = [];
+
+  let fillStructureRows = true;
+
+  for (let i = 0; i < rows.length; i++) {
+    const row = rows[i].trimEnd();
+
+    // row is empty
+    if (!row) {
+      fillStructureRows = !fillStructureRows;
+      continue;
+    }
+
+    if (fillStructureRows) {
+      const totalBoxes = Math.ceil(row.length / 4);
+
+      for (let j = 0; j < totalBoxes; j++) {
+        const pos = j + 1 + j * 3;
+        const letter = row[pos].trim();
+        const isCrate = row[pos - 1] === "[" && row[pos + 1] === "]";
+
+        if (!letter || !isCrate) {
+          continue;
+        }
+
+        if (!structure[j]) {
+          structure[j] = [];
+        }
+
+        structure[j].unshift(letter);
+      }
+    } else {
+      const match = row.match(
+        /move (?<amount>[0-9]+) from (?<from>[0-9]+) to (?<to>[0-9]+)/i
+      );
+
+      if (!match) {
+        console.warn("WARN: invalid instruction at line:", i, `"${row}"`);
+        continue;
+      }
+
+      const instruction: Instruction = {
+        amount: parseInt(match?.groups?.amount as string),
+        from: parseInt(match?.groups?.from as string),
+        to: parseInt(match?.groups?.to as string),
+      };
+
+      instructions.push(instruction);
+    }
+  }
+
+  return [structure, instructions];
+}
+
+const contents: string = fs.readFileSync(filePath, "utf-8");
+const [rows, instructions] = parseInput(contents);
+
+console.log(rows, instructions);
